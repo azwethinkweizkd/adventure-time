811ff902fc68c3544dc74555569eada08952e086
Vanessa Bonilha <81935432+bonilhav@users.noreply.github.com>
2021-10-02 11:23:22 -0600

    test

---
+++ client/src/components/Comments/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
+const Comments = () => {
+
+    return (
+        <Card sx={{ minWidth: 275, m: 3}} >
+            <CardContent>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    Word of the Day
+                </Typography>
+                <Typography variant="h5" component="div">
+                    benevolent
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    adjective
+                </Typography>
+                <Typography variant="body2">
+                    well meaning and kindly.
+                    <br />
+                    {'"a benevolent smile"'}
+                </Typography>
+            </CardContent>
+        </Card>
+    );
+};
+
+export default Comments;
