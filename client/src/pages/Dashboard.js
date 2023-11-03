9009d9c34c09a9396e1a3eaeca9d4f55e4b846e3
Kevin Devlin <azwethinkweizkd@gmail.com>
2023-11-03 14:23:00 -0600

    don't know how to fix heroku error, desperately trying anything

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,59 +1,62 @@
-import React from 'react';
-import { Redirect, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import AboutMeDashboard from '../components/AboutMeDashboard';
-import Badges from '../components/Badges';
+import React from "react";
+import { Redirect, useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import AboutMeDashboard from "../components/AboutMeDashboard";
+import Badges from "../components/Badges";
 
-import { QUERY_SINGLE_PROFILE, QUERY_ME } from '../utils/queries';
+import { QUERY_SINGLE_PROFILE, QUERY_ME } from "../utils/queries";
 
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
 
 const Dashboard = () => {
-    const { profileId } = useParams();
+  const { profileId } = useParams();
 
-    // If there is no `profileId` in the URL as a parameter, execute the `QUERY_ME` query instead for the logged in user's information
-    const { loading, data } = useQuery(
-        profileId ? QUERY_SINGLE_PROFILE : QUERY_ME,
-        {
-            variables: { profileId: profileId },
-        }
-    );
-
-    // Check if data is returning from the `QUERY_ME` query, then the `QUERY_SINGLE_PROFILE` query
-    const profile = data?.me || data?.profile || {};
-
-    // Use React Router's `<Redirect />` component to redirect to personal profile page if username is yours
-    if (Auth.loggedIn() && Auth.getProfile().data._id === profileId) {
-        return <Redirect to="/me" />;
+  // If there is no `profileId` in the URL as a parameter, execute the `QUERY_ME` query instead for the logged in user's information
+  const { loading, data } = useQuery(
+    profileId ? QUERY_SINGLE_PROFILE : QUERY_ME,
+    {
+      variables: { profileId: profileId },
     }
+  );
 
-    if (loading) {
-        return <div>Loading...</div>;
-    }
+  // Check if data is returning from the `QUERY_ME` query, then the `QUERY_SINGLE_PROFILE` query
+  const profile = data?.me || data?.profile || {};
 
-    if (!profile?.name) {
-        return (
-            <h4>
-                You need to be logged in to see your profile page. Use the navigation
-                links above to sign up or log in!
-            </h4>
-        );
-    }
+  // Use React Router's `<Redirect />` component to redirect to personal profile page if username is yours
+  if (Auth.loggedIn() && Auth.getProfile().data._id === profileId) {
+    return <Redirect to="/me" />;
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!profile?.name) {
     return (
-        <div class="flex">
-            <Badges
-                activities={profile.activities}
-                comments={profile.activities.comments}
-                isLoggedInUser={!profileId && true}
-            />
-            <AboutMeDashboard
-                residency={profile.residency}
-                myActivities={profile.myActivities}
-                favoritePlaces={profile.favoritePlaces}
-                futurePlaces={profile.futurePlaces}
-                isLoggedInUser={!profileId && true} />
-        </div>
+      <h4>
+        You need to be logged in to see your profile page. Use the navigation
+        links above to sign up or log in!
+      </h4>
     );
+  }
+  return (
+    <div class="flex">
+      <>
+        <Badges
+          activities={profile.activities}
+          comments={profile.activities.comments}
+          isLoggedInUser={!profileId && true}
+        />
+        <AboutMeDashboard
+          residency={profile.residency}
+          myActivities={profile.myActivities}
+          favoritePlaces={profile.favoritePlaces}
+          futurePlaces={profile.futurePlaces}
+          isLoggedInUser={!profileId && true}
+        />
+      </>
+    </div>
+  );
 };
 
 export default Dashboard;
